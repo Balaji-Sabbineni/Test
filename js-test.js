f8775e32fe8e955cf1d07cc4d0da146e2dbdad27
Balaji Sabbineni <balajisabbineni@gmail.com>
2025-03-16 10:45:49 +0530

    question 8 find second largest in array

--- js-test.js
+++ js-test.js
@@ -70,3 +70,10 @@ function areAnagrams(str1, str2) {
     return true
 }
 console.log(areAnagrams("listen", "silent")); // Expected output: true
+
+
+function secondLargest(arr) {
+    arr.sort();
+    return arr.at(-2)
+}
+console.log(secondLargest([10, 20, 4, 45, 99])); // Expected output: 45
