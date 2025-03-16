35f91f99f7b5eefe75ce2b03f59e263f58f646d0
Balaji Sabbineni <balajisabbineni@gmail.com>
2025-03-16 10:14:17 +0530

    question 2 palindromeStr

--- js-test.js
+++ js-test.js
@@ -6,3 +6,13 @@ function reverseString(str) {
     return rev;
 }
 console.log(reverseString("hello")); // Expected output: "olleh"
+
+function isPalindrome(str) {
+    let revStr = reverseString(str);
+    if(str == revStr){
+        return true
+    }
+    return false
+}
+console.log(isPalindrome("racecar")); // Expected output: true
+console.log(isPalindrome("hello")); // Expected output: false
